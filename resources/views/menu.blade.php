a4c4dd7eba4a7b145876655b8f0a4fc8dedcba71
Dieter Coopman <dieter@deltasolutions.be>
2021-04-10 17:36:40 +0200

    add link to logo
    Signed-off-by: Dieter Coopman <dieter@deltasolutions.be>

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -30,8 +30,8 @@
             </div>
             <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div class="flex-shrink-0 flex items-center mt-5">
-                    <img class="block lg:hidden h-8 w-auto" src="{{ asset('img/headerlogo.png') }}" alt="Workflow">
-                    <img class="hidden lg:block h-8 w-auto" src="{{ asset('img/headerlogo.png') }}" alt="Workflow">
+                    <a href="/"><img class="block lg:hidden h-8 w-auto" src="{{ asset('img/headerlogo.png') }}" alt="Workflow"></a>
+                    <a href="/"><img class="hidden lg:block h-8 w-auto" src="{{ asset('img/headerlogo.png') }}" alt="Workflow"></a>
                 </div>
                 <div class="hidden sm:ml-6 sm:flex sm:space-x-8">
                     <a href="/" class="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
