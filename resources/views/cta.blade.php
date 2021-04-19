6158c5d9ca68230260907dbcd57fd7010127a478
Dieter Coopman <dieter@deltasolutions.be>
2021-04-19 13:57:37 +0200

    add ga

--- resources/views/cta.blade.php
+++ resources/views/cta.blade.php
@@ -42,3 +42,13 @@
 
     </div>
 </div>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-194448073-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-194448073-1');
+</script>
